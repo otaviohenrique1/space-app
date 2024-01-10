f3484654c4e9acca21acfce9f696fb129cb6a609
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-01-09 22:16:08 -0300

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import BarraLateral from "./componentes/BarraLateral";
 import Banner from "./componentes/Banner";
 import bannerBackground from './assets/banner.png';
 import Galeria from "./componentes/Galeria";
+import fotos from './fotos.json'
+import { useState } from "react";
+import ModalZoom from "./componentes/ModalZoom";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -29,7 +32,11 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `;
 
-function App() {
+const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
+  const [fotoSelecionada, setFotoSelecionada] = useState(null);
+
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -42,10 +49,14 @@ function App() {
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
-            <Galeria />
+            <Galeria
+              aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+              fotos={fotosDaGaleria}
+            />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
+      <ModalZoom foto={fotoSelecionada}/>
     </FundoGradiente>
   )
 }
---
+++ src/componentes/ModalZoom/index.jsx
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+import Imagem from "../Galeria/Imagem";
+import styled from "styled-components";
+import BotaoIcone from "../BotaoIcone"
+
+// Fundo do modal
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const DialogEstilizado = styled.dialog`
+  position: absolute;
+  top: 294px;
+  background: transparent;
+  padding: 0;
+  border: 0;
+  width: 1156px;
+  display: flex;
+  justify-content: center;
+  form {
+    button {
+      position: relative;
+      top: 20px;
+      right: 60px;
+    }
+  }
+`;
+
+const ModalZoom = ({ foto }) => {
+  return (
+    <>
+      {foto && <>
+        <Overlay />
+        <DialogEstilizado open={!!foto}>
+          <Imagem foto={foto} expandida={true} />
+          <form method="dialog">
+            <BotaoIcone formMethod="dialog">
+              <img src="/icones/fechar.png" alt="Icone de fechar" />
+            </BotaoIcone>
+          </form>
+        </DialogEstilizado>
+      </>}
+    </>
+  );
+};
+
+ModalZoom.propTypes = {
+  foto: PropTypes.object,
+}
+
+export default ModalZoom;
